4ec2b0104050d7001133dc713ba64aadc840a225
Ushecoder54 <thatwhite@gmail.com>
2022-06-07 13:07:01 -0700

    Question 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,29 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-} else if (n == 0)
-{
-_putchar(48);
-return (0);
-} else (n < 0)
-{
-_putchar('-');
-}																	return (-1);
 }
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
