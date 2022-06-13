2246f34e6502804fbf43769e3d43534f3fb3abf0
Ushecoder54 <thatwhite@gmail.com>
2022-06-13 10:49:32 -0700

    Question 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,18 @@
  *
  *Return: void which means it is correct
  */
+
 void puts2(char *str)
-{int a;	
-for (a = 0; str[a] != '\0'; a++)
 {
-if (a % 2 == 0)
-_putchar (str[a]);
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar (str[a]);
+	}
+
+	_putchar ('\n');
 }
-_putchar ('\n');
-}
+
+
