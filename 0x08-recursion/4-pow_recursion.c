c9548ce059c464ca123d19510ad2e3f4de1eb493
Ushecoder54 <thatwhite@gmail.com>
2022-06-22 04:44:15 -0700

    Question 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * @x: first input
+ * @y: second input
+ * Return: Always 0 (Success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
