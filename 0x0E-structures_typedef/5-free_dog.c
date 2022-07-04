8231ef9ff53ae8ddb119c17f2e905e922d894b4b
Ushecoder54 <thatwhite@gmail.com>
2022-07-04 03:24:09 -0700

    Structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog memory
+ * @d: dog
+ */
+
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+	return;
+
+free(d->owner);
+free(d->name);
+free(d);
+}
+Footer
+
