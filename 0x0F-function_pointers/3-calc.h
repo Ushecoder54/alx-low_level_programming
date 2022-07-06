494923b982432682f1fe22cbe46649f3623d2082
Ushecoder54 <thatwhite@gmail.com>
2022-07-06 12:19:22 -0700

    Qsn

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,13 @@
 #ifndef CALC_H
 #define CALC_H
 
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * struct op - Struct op
  * @op: The operator
@@ -13,11 +20,4 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
-#endif/* _CALC_H_ */
+#endif /* _CALC_H_ */
