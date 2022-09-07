c2b95498732b4228762e777733944e51f883be31
Ushecoder54 <thatwhite@gmail.com>
2022-09-07 02:11:31 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - a fxn that computes absolute val of int
+ * @ab: interger input
+ * Return: absolute val of ab
+ */
+int _abs(int ab)
+{
+return (ab * ((ab > 0) - (ab < 0)));
+}
---
+++ 0x18-dynamic_libraries/isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
+
