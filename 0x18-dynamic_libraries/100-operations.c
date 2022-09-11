1306d605622999e1fa4f0d3315dde18542155c81
Ushecoder54 <thatwhite@gmail.com>
2022-09-11 14:17:58 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,29 @@
+Int add(int a, int b)
+{
+      
+      return (a + b);
+
+}
+
+Int add(int a, int b)
+{
+      
+      return (a - b);
+
+}
+
+
+Int add(int a, int b)
+{
+      
+      return (a * b);
+
+}
+
+
+Int add(int a, int b)
+{
+      
+      return (a / b);
+
+}
